092dc9c69f8802b9d88e3278605d5d83547984a5
Jokūbas Toločka <jokubas.tj@gmail.com>
2020-01-08 21:23:42 +0200

    fix hopefully final

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,12 @@
 // require('dotenv').config();
 var jwt = require("jsonwebtoken");
+const config = require("config");
 
 // make sure the user is logged - Authentication
 exports.loginRequired = function(req, res, next) {
     try {
         const token = req.headers.authorization.split(" ")[1]; // Bears token
-        jwt.verify(token, process.env.SECURE_KEY, function(err, decoded) {
+        jwt.verify(token, config.get('jwtSecret'), function(err, decoded) {
         if(decoded) {
             next();
         } else {
@@ -27,7 +28,7 @@ exports.loginRequired = function(req, res, next) {
 exports.ensureCorrectUser = function(req, res, next) {
     try {
       const token = req.headers.authorization.split(" ")[1];
-      jwt.verify(token, process.env.SECURE_KEY, function(err, decoded) {
+      jwt.verify(token, config.get('jwtSecret'), function(err, decoded) {
         if(decoded && decoded.id === req.params.id) {
           return next();
         } else {
