8ef9901ea9224dd8a8928dc5c0dcfdb8748602ad
Jokūbas Toločka <jokubas.tj@gmail.com>
2020-01-09 10:38:53 +0200

    made devtools a function

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,13 +1,16 @@
 import rootReducer from './reducers';
-import {createStore, applyMiddleware} from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
+let devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && 
+window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+    devTools = a => a;
+}
 
 export function configureStore(){
     const store = createStore(rootReducer,
-        composeWithDevTools(applyMiddleware(thunk),
+        compose(applyMiddleware(thunk),
          devTools));
     return store;
 }
